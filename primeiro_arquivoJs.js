b50f66225c0f2528d92a30dca9d4467e380af17b
Cleiton Ferreira <cleitonads16@gmail.com>
2021-04-15 17:44:43 -0300

    Teste Loops

--- primeiro_arquivoJs.js
+++ primeiro_arquivoJs.js
@@ -1,1 +1,26 @@
-console.log('Primeiro Teste NodeJs');
+let dia_semana = 3;
+let dia = "";
+
+switch(dia_semana){
+    case 0:
+        dia ='segunda'
+        break
+    case 1:
+        dia ='terça'
+        break
+    case 2:
+        dia ='quarta'
+        break
+    case 3:
+        dia ='quinta'
+        break
+    case 4:
+        dia ='sexta'
+        break
+    case 5:
+        dia ='sábado'
+        break
+    default:
+        dia = '--'
+}
+console.log('Hoje é '+ dia);
